64886d153aaf9323926f5325316b7adcf56c8084
letvarprint <a.brixencov@icloud.com>
2025-03-25 18:45:32 +0200

    Удалил лишние отступы

---
+++ ExampleApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by Alexandru Brixencov on 24.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
